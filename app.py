97515ad2c58b510d34dc1e1736af4f8d168c8fbf
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2023-12-14 09:58:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
             hill_result = typer.style(hillEncrypt(hill_txt,hill_code),fg=typer.colors.BLUE)
             hill_code = typer.style(hill_code,fg=typer.colors.RED)
             hill_table = [
-                [f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
-                [f"{hill_code}",f"{hill_result}"],
+                [f"{typer.style('Asl matn',fg=typer.colors.GREEN)}",f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
+                [f"{typer.style(hill_txt,fg=typer.colors.BLUE)}",f"{hill_code}",f"{hill_result}"],
             ]
             typer.secho("Sizning \"HILL\" usulida matningiz shifrlandi",fg=typer.colors.CYAN,bold=True)
             print(tabulate(hill_table,headers="firstrow",tablefmt="psql"))
@@ -40,8 +40,8 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
             hill_result = typer.style(hillDecrypt(hill_txt,hill_code),fg=typer.colors.BLUE)
             hill_code = typer.style(hill_code,fg=typer.colors.RED)
             hill_table = [
-                [f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
-                [f"{hill_code}",f"{hill_result}"],
+                [f"{typer.style('Shifrlangan matn',fg=typer.colors.GREEN)}",f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
+                [f"{typer.style(hill_txt,fg=typer.colors.BLUE)}",f"{hill_code}",f"{hill_result}"],
             ]
             typer.secho("Sizning \"HILL\" usulida matningiz shifrdan chiqarildi",fg=typer.colors.CYAN,bold=True)
             print(tabulate(hill_table,headers="firstrow",tablefmt="psql"))
@@ -79,8 +79,8 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
             caesar_num = typer.prompt("1dan 27gacha bo'lgan sonlardan birini yuboring")
             caesar_res = typer.style(caesar_encipher(caesar_txt,caesar_num),fg=typer.colors.BLUE)
             caesar_table = [
-                [f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
-                [f"{typer.style(caesar_num,fg=typer.colors.RED)}",f"{caesar_res}"],
+                [f"{typer.style('Asl matn',fg=typer.colors.GREEN)}",f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
+                [f"{typer.style(caesar_txt,fg=typer.colors.BLUE)}"f"{typer.style(caesar_num,fg=typer.colors.RED)}",f"{caesar_res}"],
             ]
             typer.secho("Sizning \"Sizar\" usulida matningiz shifrlandi chiqarildi",fg=typer.colors.CYAN,bold=True)
             print(tabulate(caesar_table,headers="firstrow",tablefmt="psql"))
@@ -93,8 +93,8 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
             caesar_num = typer.prompt("Shiifrdan chiqarish uchun 1dan 27gacha bo'lgan son yuboring")
             caesar_res = typer.style(caesar_decipher(caesar_txt,caesar_num),fg=typer.colors.BLACK)
             caesar_table = [
-                [f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
-                [f"{typer.style(caesar_num,fg=typer.colors.RED)}",f"{caesar_res}"],
+                [f"{typer.style('Shifr matni',fg=typer.colors.GREEN)},"f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
+                [f"{typer.style(caesar_txt,fg=typer.colors.BLUE)}",f"{typer.style(caesar_num,fg=typer.colors.RED)}",f"{caesar_res}"],
             ]
             typer.secho("Sizning \"Sizar\" usulida matningiz shifrdan chiqarildi",fg=typer.colors.CYAN,bold=True)
             print(tabulate(caesar_table,headers="firstrow",tablefmt="psql"))
@@ -132,8 +132,8 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
             hill_result = typer.style(hillEncrypt(hill_txt,hill_code),fg=typer.colors.BLUE)
             hill_code = typer.style(hill_code,fg=typer.colors.RED)
             hill_table = [
-                [f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
-                [f"{hill_code}",f"{hill_result}"],
+                [f"{typer.style('Asl matn',fg=typer.colors.GREEN)}",f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
+                [f"{typer.style(hill_txt,fg=typer.colors.BLUE)}",f"{hill_code}",f"{hill_result}"],
             ]
             typer.secho("Sizning \"HILL\" usulida matningiz shifrlandi",fg=typer.colors.CYAN,bold=True)
             print(tabulate(hill_table,headers="firstrow",tablefmt="psql"))
@@ -148,8 +148,8 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
             hill_result = typer.style(hillDecrypt(hill_txt,hill_code),fg=typer.colors.BLUE)
             hill_code = typer.style(hill_code,fg=typer.colors.RED)
             hill_table = [
-                [f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f{typer.style('Natija',fg=typer.colors.GREEN)}"],
-                [f"{hill_code}",f"{hill_result}"],
+                [f"{typer.style('Shifr matni',fg=typer.colors.GREEN)}",f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
+                [f"{typer.style(hill_txt,fg=typer.colors.BLUE)}",f"{hill_code}",f"{hill_result}"],
             ]
             typer.secho("Sizning \"HILL\" usulida matningiz shifrdan chiqarildi",fg=typer.colors.CYAN,bold=True)
             print(tabulate(hill_table,headers="firstrow",tablefmt="psql"))
@@ -187,8 +187,8 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
             caesar_num = typer.prompt("Send one of the numbers from 1 to 27")
             caesar_res = typer.style(caesar_encipher(caesar_txt,caesar_num),fg=typer.colors.BLUE)
             caesar_table = [
-                [f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
-                [f"{typer.style(caesar_num,fg=typer.colors.RED)}",f"{caesar_res}"],
+                [f"{typer.style('Asl matn',fg=typer.colors.GREEN)}",f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
+                [f"{typer.style(caesar_txt,fg=typer.colors.BLUE)}"f"{typer.style(caesar_num,fg=typer.colors.RED)}",f"{caesar_res}"],
             ]
             typer.secho("Sizning \"Sizar\" usulida matningiz shifrlandi chiqarildi",fg=typer.colors.CYAN,bold=True)
             print(tabulate(caesar_table,headers="firstrow",tablefmt="psql"))
@@ -235,15 +235,15 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
 
 
 ##################### This section about Russian ##################################################
-    elif x == "hill" and mode == "m--encode" and lang == "uz":
+    elif x == "hill" and mode == "m--encode" and lang == "ru":
         try:
             hill_txt = typer.prompt("Shiflamoqchi bo'lgan matningizni yuboring")
             hill_code = typer.prompt("Sifringizga parol qo'ying")
             hill_result = typer.style(hillEncrypt(hill_txt,hill_code),fg=typer.colors.BLUE)
             hill_code = typer.style(hill_code,fg=typer.colors.RED)
             hill_table = [
-                [f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
-                [f"{hill_code}",f"{hill_result}"],
+                [f"{typer.style('Asl matn',fg=typer.colors.GREEN)}",f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
+                [f"{typer.style(hill_txt,fg=typer.colors.BLUE)}",f"{hill_code}",f"{hill_result}"],
             ]
             typer.secho("Sizning \"HILL\" usulida matningiz shifrlandi",fg=typer.colors.CYAN,bold=True)
             print(tabulate(hill_table,headers="firstrow",tablefmt="psql"))
@@ -251,7 +251,7 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
             typer.secho("siz xatoga yo'l qo'ydingin foydalanish qo'llanmasini qayta qarang",fg=typer.colors.RED)
 
 
-    elif x == "hill" and mode == "m--decode" and lang == "uz":
+    elif x == "hill" and mode == "m--decode" and lang == "ru":
         try:
             hill_txt = typer.prompt("Shifrdan chiqarmoqchi bo'lgan matningizni yuboring")
             hill_code = typer.prompt("Sifr parolini ayting")
@@ -267,7 +267,7 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
         except ZeroDivisionError:
             typer.secho("siz xatoga yo'l qo'ydingin foydalanish qo'llanmasini qayta qarang",fg=typer.colors.RED)
 
-    elif x == "morse" and mode == "m--encode" and lang == "uz":
+    elif x == "morse" and mode == "m--encode" and lang == "ru":
         try:
             morse_txt = typer.prompt("Kodlash uchun matningizni yuboring ")
             morse_result = typer.style(text_to_morse(morse_txt),fg=typer.colors.BLUE)
@@ -279,7 +279,7 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
             print(tabulate(morse_table,headers="firstrow",tablefmt="psql"))
         except:
             typer.secho("siz xatoga yo'l qo'ydingin foydalanish qo'llanmasini qayta qarang",fg=typer.colors.RED)
-    elif x == "morse" and mode == "m--decode" and lang == "uz":
+    elif x == "morse" and mode == "m--decode" and lang == "ru":
         try:
             morse_txt = typer.prompt("Shifrdan chiqarmoqchi bo'lgan matningizni yuboring")
             morse_result = typer.style(morse_to_text(morse_txt),fg=typer.colors.RED)
@@ -291,34 +291,34 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
             print(tabulate(morse_table,headers="firstrow",tablefmt="psql"))
         except:
             typer.secho("siz xatoga yo'l qo'ydingin foydalanish qo'llanmasini qayta qarang",fg=typer.colors.RED)
-    elif x == "caesar" and mode == "m--encode" and lang == "uz":
+    elif x == "caesar" and mode == "m--encode" and lang == "ru":
         try:
             caesar_txt = typer.prompt("Sifrlamoqchi bo'lgan matningizni bering")
             caesar_num = typer.prompt("1dan 27gacha bo'lgan sonlardan birini yuboring")
             caesar_res = typer.style(caesar_encipher(caesar_txt,caesar_num),fg=typer.colors.BLUE)
             caesar_table = [
-                [f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
-                [f"{typer.echo(caesar_num,fg=typer.colors.RED)}",f"{caesar_res}"],
+                [f"{typer.style('Asl matn',fg=typer.colors.GREEN)}",f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
+                [f"{typer.style(caesar_txt,fg=typer.colors.BLUE)}"f"{typer.style(caesar_num,fg=typer.colors.RED)}",f"{caesar_res}"],
             ]
             typer.secho("Sizning \"Sizar\" usulida matningiz shifrlandi chiqarildi",fg=typer.colors.CYAN,bold=True)
             print(tabulate(hill_table,headers="firstrow",tablefmt="psql"))
         except:
             typer.secho("siz xatoga yo'l qo'ydingin foydalanish qo'llanmasini qayta qarang",fg=typer.colors.RED)
         
-    elif x == "caesar" and mode == "m--decode" and lang == "uz":
+    elif x == "caesar" and mode == "m--decode" and lang == "ru":
         try:
             caesar_txt = typer.prompt("Shifrdan chiqarmoqchi bo'lgan matningizni yuboring")
             caesar_num = typer.prompt("Shiifrdan chiqarish uchun 1dan 27gacha bo'lgan son yuboring")
             caesar_res = typer.style(caesar_decipher(caesar_txt,caesar_num),fg=typer.colors.BLACK)
             caesar_table = [
-                [f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
-                [f"{typer.echo(caesar_num,fg=typer.colors.RED)}",f"{caesar_res}"],
+                [f"{typer.style('Shifr matni',fg=typer.colors.GREEN)},"f"{typer.style('Shifr paroli',fg=typer.colors.GREEN)}",f"{typer.style('Natija',fg=typer.colors.GREEN)}"],
+                [f"{typer.style(caesar_txt,fg=typer.colors.BLUE)}",f"{typer.style(caesar_num,fg=typer.colors.RED)}",f"{caesar_res}"],
             ]
             typer.secho("Sizning \"Sizar\" usulida matningiz shifrdan chiqarildi",fg=typer.colors.CYAN,bold=True)
             print(tabulate(hill_table,headers="firstrow",tablefmt="psql"))
         except:
             typer.secho("siz xatoga yo'l qo'ydingin foydalanish qo'llanmasini qayta qarang",fg=typer.colors.RED)
-    elif x == "mirage" and  mode == "m--encode" and lang == "uz":
+    elif x == "mirage" and  mode == "m--encode" and lang == "ru":
         try:
             mirage_txt = typer.prompt("Shifrlamoqchi bo'lgan matningizni yuboring")
             mirage_res = typer.style(en_mirage(mirage_txt),fg=typer.colors.RED)
@@ -330,12 +330,12 @@ def main(x: Optional[str] = typer.Argument('home',help="Section"),mode: Optional
             print(tabulate(mirage_table,headers="firstrow",tablefmt="psql"))
         except:
             typer.secho("siz xatoga yo'l qo'ydingin foydalanish qo'llanmasini qayta qarang",fg=typer.colors.RED)
-    elif x == "mirage" and mode == "m--decode" and lang == "uz":
+    elif x == "mirage" and mode == "m--decode" and lang == "ru":
         try:
             mirage_txt = typer.prompt("Shifrdan chiqarmoqchi bo'lgan matningizni yuboring")
             mirage_res = typer.style(de_mirage(mirage_txt),fg=typer.colors.RED)
             mirage_table = [
-                [f"Asl matn",f"Kodlangan matn"]
+                [f"Kodlangan matn",f"Natija"]
                 [f"{typer.style(mirage_txt,fg=typer.colors.RED)}",f"{mirage_res}"]
             ]
             typer.echo(f"Sizning matningiz \"Mirage\"usulida koddan chiqarildi",fg=typer.colors.CYAN,bold=True)
